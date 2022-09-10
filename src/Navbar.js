bc93c9c0876477a7024e2cbed8cbcfa93f22b630
UkemeSkywalker <skywalkershadow9@gmail.com>
2022-09-10 03:50:47 +0100

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,13 @@
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <h1>The Skywalker Blog</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Post</a>
-            </div>
-
-        </nav>
-     );
+  return (
+    <nav className="navbar flex flex-row place-content-center pt-4 pb-3 justify-evenly">
+      <h1 className="ml:auto">The Skywalker Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create" className="ml-6 color bg-cyan-500 p-2 rounded" >New Post</a>
+      </div>
+    </nav>
+  )
 }
- 
-export default Navbar;
+
+export default Navbar
